34945d920943b1a7f43d78383fd3812bc2dbc365
David Nagy <se18m005@technikum-wien.at>
2018-10-06 16:36:22 +0200

    Server side tested and running.

--- mars/moon.py
+++ mars/moon.py
@@ -1,199 +1,43 @@
-# Create your models here.
+class Moon:
 
 
-class Moon(models.Model):
+	# Old attributes used in the previous implementation. Probably not needed anymore
+	# Interval = ""
 
-	moon_id = models.AutoField(primary_key=True)
-
-	dInterval = models.TextField(default='')
-	pInterval = models.TextField(default='')
-
-	dRiseHour = models.PositiveIntegerField(default=12)
-	dRiseMinute = models.PositiveIntegerField(default=12)
-	dDescentHour = models.PositiveIntegerField(default=12)
-	dDescentMinute = models.PositiveIntegerField(default=12)
+	#riseHour = 0
+	#riseMinute = 0
+	#descentHour = 0
+	#descentMinute = 0
 	
-	dRiseTime = models.PositiveIntegerField(null = True)
-	dDescentTime = models.PositiveIntegerField(null = True)
+	riseTime = 0
+	descentTime = 0
 
-	pRiseHour = models.PositiveIntegerField(default=12)
-	pRiseMinute = models.PositiveIntegerField(default=12)
-	pDescentHour = models.PositiveIntegerField(default=12)
-	pDescentMinute = models.PositiveIntegerField(default=12)
-	
-	pRiseTime = models.PositiveIntegerField(null = True)
-	pDescentTime = models.PositiveIntegerField(null = True)
+	def __init__(self, inputRiseHour, inputRiseMin, inputDescentHour, inputDescentMin):
+		#Process direct input
+		self.riseTime = inputRiseHour* 100 + inputRiseMin
 
-	overlap = models.PositiveIntegerField(null = True)
+		self.descentTime = inputDescentHour * 100 + inputDescentMin
 
-###
-#GETTERS
-###
+		if self.descentTime < self.riseTime:
+			# The descent takes place on the next day
+			self.descentTime += 2500
 
-	def get_dRiseHour(self):
-		return self.dRiseHour
+	# ##
+	# GETTER METHODS
+	# ##
 
-	def get_dRiseMinute(self):
-		return self.dRiseMinute
+	def get_rise_time(self):
+		return self.riseTime
 
-	def get_dDescentHour(self):
-		return self.dDescentHour
+	def get_descent_time(self):
+		return self.descentTime
 
-	def get_dDescendMinute(self):
-		return self.dDescentMinute
+	###
+	# SETTER METHODS
+	###
 
-	def get_pRiseHour(self):
-		return self.pRiseHour
+	def set_rise_time(self, riseTime):
+		self.riseTime = riseTime
 
-	def get_pRiseMinute(self):
-		return self.pRiseMinute
-
-	def get_pDescendHour(self):
-		return self.pDescentHour
-
-	def get_pDescendMinute(self):
-		return self.pDescentMinute
-
-	def get_overlap(self):
-		return self.overlap
-
-###
-# SETTERS
-###
-
-	def set_dRiseHour(self, riseHour):
-		self.dRiseHour = riseHour
-
-	def set_dRiseMinute(self, riseMinute):
-		self.dRiseMinute = riseMinute
-
-	def set_dDescentHour(self, descentHour):
-		self.dDescentHour = descentHour
-
-	def set_dDescentMinute(self, descentMinute):
-		self.dDescentMinute = descentMinute
-
-	def set_pRiseHour(self, riseHour):
-		self.pRiseHour = riseHour
-
-	def set_pRiseMinute(self, riseMinute):
-		self.pRiseMinute = riseMinute
-
-	def set_pDescentHour(self, descentHour):
-		self.pDescentHour = descentHour
-
-	def set_pDescentMinute(self, descentMinute):
-		self.pDescentMinute = descentMinute
-
-	def set_overlap(self, overlap):
-		self.overlap = overlap
-###
-# Methods
-###
-
-
-	def process_periods(self):
-
-		(dIntervalRise,dIntervalDescent) = self.dInterval.strip().strip('()').strip('[]').split(',')
-		(pIntervalRise,pIntervalDescent) = self.pInterval.strip().strip('()').strip('[]').split(',')
-
-		(dHourRise, dMinuteRise) = dIntervalRise.split(':')
-
-		self.set_dRiseHour(int(dHourRise))
-		self.set_dRiseMinute(int(dMinuteRise))
-
-		(dHourDescent, dMinuteDescent) = dIntervalDescent.split(':')
-
-		self.set_dDescentHour(int(dHourDescent))
-		self.set_dDescentMinute(int(dMinuteDescent))
-
-		(pHourRise, pMinuteRise) = pIntervalRise.split(':')
-
-		self.set_pRiseHour(int(pHourRise))
-		self.set_pRiseMinute(int(pMinuteRise))
-
-		(pHourDescent, pMinuteDescent) = pIntervalDescent.split(':')
-
-		self.set_pDescentHour(int(pHourDescent))
-		self.set_pDescentMinute(int(pMinuteDescent))
-
-
-	def generalise_time(self):
-		# For Deimos
-
-		self.dRiseTime = self.dRiseHour * 100 + self.dRiseMinute
-
-		self.dDescentTime = self.dDescentHour * 100 + self.dDescentMinute
-
-		if self.dDescentTime < self.dRiseTime:
-			# The descent takes place on the nextday
-			self.dDescentTime += 2500
-
-		# For Phobos
-		self.pRiseTime = self.pRiseHour * 100 + self.pRiseMinute
-		self.pDescentTime = self.pDescentHour * 100 + self.pDescentMinute
-
-		if self.pDescentTime < self.pRiseTime:
-			# The descent takes place on the nextday
-			self.pDescentTime += 2500
-
-	def calculate_overlap(self):
-
-	# Deimos rises before Phobos
-
-		if self.dRiseTime < self.pRiseTime:
-
-			# Case 1
-			# Demios descends before Phobos raises
-
-			if self.dDescentTime < self.pRiseTime:
-				self.set_overlap(0)
-
-			# Case 2
-
-			elif self.dDescentTime <= self.pDescentTime:
-				self.set_overlap((self.dDescentTime - self.pRiseTime))
-
-			# Case 3
-
-			elif self.pDescentTime <= self.dDescentTime:
-				self.set_overlap((self.pDescentTime - self.pRiseTime))
-
-			# Exc. 1
-			elif self.dDescentTime == self.pRiseTime:
-				self.set_overlap(1)
-	
-		# Phobos rises before Deimos
-		if self.pRiseTime < self.dRiseTime:
-
-			# Case 4
-			if self.pDescentTime < self.dRiseTime:
-				self.set_overlap(0)
-		
-			# Case 5
-			elif self.pDescentTime <= self.dDescentTime:
-				self.set_overlap((self.pDescentTime - self.dRiseTime))
-		
-			# Case 6
-			elif self.dDescentTime <= self.pDescentTime:
-				self.set_overlap((self.dDescentTime - self.dRiseTime))
-
-			# Exc. 2
-			elif self.pDescentTime == self.dRiseTime:
-				self.set_overlap(1)
-
-		# If they rise at the same time
-		if self.pRiseTime == self.dRiseTime:
-
-			# If Deimos descends earlier
-
-			if self.dDescentTime < self.pDescentTime:
-				self.set_overlap((self.dDescentTime - self.dRiseTime))
-
-
-			# If Phobos descends earlier or at the same time
-
-			if self.pRiseTime <= self.dRiseTime:
-				self.set_overlap((self.pDescentTime - self.pRiseTime))
-
-		return self.overlap
+	def set_descent_time(self, descentTime):
+		self.descentTime = descentTime
--- mars/rest_server.py
+++ mars/rest_server.py
@@ -1,77 +1,156 @@
+#
+# IMPORT Routines
+#
+
 from flask import Flask, jsonify, abort, make_response, request
 from flask_restful import Api, Resource, reqparse, fields, marshal
-
 from mars.moon import Moon
 
-app = Flask(__name__, statuc_url_path="")
+#
+# Define global instances, required for Flask and its REST API
+#
+app = Flask(__name__, static_url_path="")
 api = Api(app)
 
 
+def calculate_overlap(deimos, phobos):
+    """
+    Function that takes two inputs of the moon class in order to calculate the overlap
+    This is done by taking running through a set of if-causes to determine the correct scenario.
 
-def send_error_404():
-    return make_response(jsonify({'Error': 'This page was not found!'}), 404)
+    :param deimos: Instance of moon class
+    :param phobos: Instance of moon class
+    :return: The overlap of MArs minutes in an integer
+    """
+
+    # Deimos rises before Phobos
+
+    if deimos.riseTime < phobos.riseTime:
+        # Exc. 1
+        # Twilight Rule - when Deimos descends in the same minute Phobos ascends
+        # Results in one minute of overlap
+
+        if deimos.descentTime == phobos.riseTime:
+            return 1
+        # Case 1
+        # Deimos descends before Phobos rises
+        # Results in no overlap
+
+        elif deimos.descentTime < phobos.riseTime:
+            return 0
+
+        # Case 2
+        # Deimos descends after Phobos rose and before / same time when Phobos descended
+        # Results in an overlap between the ascent of Phobos and descent of Deimos
+
+        elif deimos.descentTime <= phobos.descentTime:
+            return deimos.descentTime - phobos.riseTime
+
+        # Case 3
+        # Deimos descends after Phobos rose and after Phobos descended.
+        # Overlap based on time between ascent and descent of Phobos
+
+        elif phobos.descentTime < deimos.descentTime:
+            return phobos.descentTime - phobos.riseTime
 
 
-def send_error_400():
-    return make_response(jsonify({'Error': 'Incorrect request!'}), 400)
+
+    # Phobos rises before Deimos
+    if phobos.riseTime < deimos.riseTime:
+
+        # Exc. 2
+        # Twilight Rule - when Deimos ascends in the same minute Phobos descends
+        # Results in one minute of overlap
+        if phobos.descentTime == deimos.riseTime:
+            return 1
+
+        # Case 4
+        # Phobos descends before Deimos rises
+        # Results in no overlap
+        elif phobos.descentTime < deimos.riseTime:
+            return 0
+
+        # Case 5
+        # Phobos descends after Deimos ascends, but before or at the same time it descends
+        # Results in an overlap between the ascent of Deimos and the descent of Phobos
+        elif phobos.descentTime <= deimos.descentTime:
+            return phobos.descentTime - deimos.riseTime
+
+        # Case 6
+        # Phobos descends after Deimos ascends and descends
+        # Results in an overlap between the the ascent of Deimos and the descent of Deimos
+        elif deimos.descentTime < phobos.descentTime:
+            return deimos.descentTime - deimos.riseTime
+
+    # If they rise at the same time
+    if phobos.riseTime == deimos.riseTime:
+
+        # Case 7
+        # If Deimos descends before Phobos
+        # Results in an overlap between the ascent of Deimos and the descent of Deimos
+        if deimos.descentTime < phobos.descentTime:
+            return deimos.descentTime - deimos.riseTime
+
+        # Case 8
+        # If Phobos descends before Deimos or at the same time
+        # Results in an overlap between the ascent of Phobos and the descent of Phobos
+
+        if phobos.riseTime <= deimos.riseTime:
+            return phobos.descentTime - phobos.riseTime
 
 
 class MoonAPI(Resource):
     def __init__(self):
+        """
+        Init method used to parse the received arguments from the browser client.
+        4 arguments per moon are received: Time of ascent in hours and minutes
+        and the time of descent in hours and minutes.
+
+        For each argument, integer is sat as required type. In case not delivered,
+        an error message is sent through the RequestParser with the provided comment.
+        """
+        #
+        # Parse request and define arguments
+        #
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('drisemin', type=int, required=True,
-                                   help='No Mars minutes provided', location='json')
-        self.reqparse.add_argument('drisehours', type=int, required=True,
-                                   help='No Mars hours provided', location='json')
-        self.reqparse.add_argument('ddescendmin', type=int, required=True,
-                                   help='No Mars minutes provided', location='json')
-        self.reqparse.add_argument('ddescendhours', type=int, required=True,
-                                   help='No Mars hours provided', location='json')
+                                   help='No Deimos ascent minutes provided', location='json')
+        self.reqparse.add_argument('drisehour', type=int, required=True,
+                                   help='No Deimos ascent hours provided', location='json')
+        self.reqparse.add_argument('ddescentmin', type=int, required=True,
+                                   help='No Deimos descent minutes provided', location='json')
+        self.reqparse.add_argument('ddescenthour', type=int, required=True,
+                                   help='No Deimos descent hours provided', location='json')
         self.reqparse.add_argument('prisemin', type=int, required=True,
-                                   help='No Mars minutes provided', location='json')
-        self.reqparse.add_argument('prisehours', type=int, required=True,
-                                   help='No Mars hours provided', location='json')
-        self.reqparse.add_argument('pdescendmin', type=int, required=True,
-                                   help='No Mars minutes provided', location='json')
-        self.reqparse.add_argument('pdescendhours', type=int, required=True,
-                                   help='No Mars hours provided', location='json')
+                                   help='No Phobos ascent minutes provided', location='json')
+        self.reqparse.add_argument('prisehour', type=int, required=True,
+                                   help='No Phobos ascent hours provided', location='json')
+        self.reqparse.add_argument('pdescentmin', type=int, required=True,
+                                   help='No Phobos descent minutes provided', location='json')
+        self.reqparse.add_argument('pdescenthour', type=int, required=True,
+                                   help='No Phobos descent hours provided', location='json')
 
-        super(MoonsAPI, self).__init__()
+        super(MoonAPI, self).__init__()
 
-        def post(self):
-            args = self.reqparse.parse_args()
-            daimos = Moon()
-            phobos = Moon()
+    def post(self):
+        """
+        The method called in case a POST request is received by the REST API
 
+        :param self: USed to call the Resource methods
+        :return: Integer value showing the overlap in minutes when both moons can be seen
+        """
+        args = self.reqparse.parse_args()
+        daimos = Moon(args['drisehour'], args['drisemin'], args['ddescenthour'], args['ddescentmin'] )
+        phobos = Moon(args['prisehour'], args['prisemin'], args['pdescenthour'], args['pdescentmin'] )
 
-
-
-@app.route('/mars/webservice/moons', methods=['POST'])
-def process_request_to_calculate_overlap():
-    if not request.json:
-        return send_error_400()
-
-
-# Create your views here.
-def main(request):
-    return render(request, 'mars/home.html')
+        return {'overlap': calculate_overlap(daimos, phobos)}
 
 
-def result(request):
-    dInterval = request.POST['deimos_interval']
-    pInterval = request.POST['phobos_interval']
-
-    moon_ = Moon.objects.create(dInterval=request.POST['deimos_interval'], pInterval=request.POST['phobos_interval'])
-    moon_.process_periods()
-    moon_.generalise_time()
-    overlap = moon_.calculate_overlap()
-    moon_.save()
-
-    return render(request, 'mars/result.html', {'overlap': overlap})
-
-
-api.add_resource(MoonsAPI), '/mars/webservice/moons', endpoint='moon')
+#
+# This function creates the URI information for the REST API
+#
+api.add_resource(MoonAPI, '/mars/webservice/moon', endpoint='moon')
 
 
 if __name__ == '__main__':
-    app.-run(debug=True)
+    app.run(debug=True)
